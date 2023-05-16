68c97038738d2ad52b7af8ad9969c63ea97942ec
ErikaJPB <erikajpinedab@gmail.com>
2023-05-16 12:21:20 -0500

    database implemented in characters microservice  as an http request

--- characters/src/controllers/getCharacters.js
+++ characters/src/controllers/getCharacters.js
@@ -2,6 +2,6 @@ const Character = require("../data");
 const { response } = require("../utils");
 
 module.exports = async (req, res) => {
-  const characters = await Character.list();
+  const characters = await axios.get("http://database:8004/Character");
   response(res, 200, characters);
 };
