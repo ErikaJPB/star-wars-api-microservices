64616e3af4b616df2bcde2dac1273a8e12b50886
ErikaJPB <erikajpinedab@gmail.com>
2023-05-18 15:44:42 -0500

    added docker compose file

--- characters/src/controllers/getCharacters.js
+++ characters/src/controllers/getCharacters.js
@@ -2,6 +2,6 @@ const Character = require("../data");
 const { response } = require("../utils");
 
 module.exports = async (req, res) => {
-  const characters = await axios.get("http://database:8004/Character");
+  const characters = await Character.list();
   response(res, 200, characters);
 };
